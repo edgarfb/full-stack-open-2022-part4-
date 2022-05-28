04226a6cbb8eed750e912ca1d41ecbcca54d0756
Edgar Britez <edgarfbritez@gmail.com>
2022-05-28 20:31:32 -0300

    4.1 Blog list, step1

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const pass = "Pfso48149";
+const dataBase = "blog-list";
+const mongoUrl = `mongodb+srv://edgarfbFSO:${pass}@cluster0.eoy65.mongodb.net/${dataBase}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("App works!!");
+});
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  console.log(request.body);
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
