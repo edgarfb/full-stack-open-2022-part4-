b05ce1a1f403587fb718adbed119e356e3e07d92
Edgar Britez <edgarfbritez@gmail.com>
2022-05-29 14:36:53 -0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
