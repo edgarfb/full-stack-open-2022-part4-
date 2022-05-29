362f3799e76084fdfdf644cc642e5b1af5f61eed
Edgar Britez <edgarfbritez@gmail.com>
2022-05-29 08:07:21 -0300

    separete module for /models/blog

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 mongoose.connect(MONGODB_URI);
 
